7781e45a14358722d9219764b84c3c57c24705b6
grizzyll <felisitasgriselda@gmail.com>
2026-06-06 23:15:20 +0700

    fitur: menambahkan fitur pendengar mqtt sukses uji coba

--- app/Console/Commands/SubscribeToMqtt.php
+++ app/Console/Commands/SubscribeToMqtt.php
@@ -8,13 +8,15 @@ use PhpMqtt\Client\ConnectionSettings;
 
 class SubscribeToMqtt extends Command
 {
+    // Ini perintah yang dijalankan di terminal
     protected $signature = 'mqtt:subscribe';
     protected $description = 'Memantau data masuk dari alat sortir fisik via MQTT';
 
     public function handle()
     {
-        $server   = 'broker.emqx.io';
-        $port     = 1883;
+        // Mengambil setingan dari file .env yang sudah kamu isi tadi
+        $server   = config('mqtt.connections.default.host', 'broker.emqx.io');
+        $port     = (int) config('mqtt.connections.default.port', 1883);
         $clientId = 'laravel_sortirin_' . rand(1, 999);
 
         $this->info("Menghubungkan ke Broker MQTT ({$server}:{$port})...");
@@ -29,10 +31,15 @@ class SubscribeToMqtt extends Command
             $mqtt->connect($settings, true);
             $this->info("Berhasil Terhubung! Menunggu data sensor...");
 
+            // Mendengarkan papan pengumuman/topic 'sortirin/sensor'
             $mqtt->subscribe('sortirin/sensor', function ($topic, $message) {
                 $this->info("Ada data masuk dari topic [{$topic}]: {$message}");
+                
+                // DI SINI KAMU BISA MASUKKAN LOGIKA SIMPAN DATA KAMU NANTI
+                
             }, 0);
 
+            // Loop terus-menerus agar VPS stand-by mendengarkan
             $mqtt->loop(true);
 
         } catch (\Exception $e) {
