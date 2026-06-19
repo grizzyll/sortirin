e72252ef265ab03a58aa262d0ba0500e5820df75
grizzyll <felisitasgriselda@gmail.com>
2026-06-20 01:13:02 +0700

    nyoba

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\RegionalController;
 use App\Models\Bin;
 
 // 1. ROUTE UTAMA
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return redirect('/welcome');
 });
 
